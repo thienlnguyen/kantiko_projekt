f92664e3fa5d79c545e11dbb852db9b7c083df82
thien <s0569834@htw-berlin.de>
2024-03-12 00:30:18 -0400

    ESLint deactivieren

--- quasar.config.js
+++ quasar.config.js
@@ -73,10 +73,10 @@ module.exports = configure(function (ctx) {
       // https://v2.quasar.dev/quasar-cli-webpack/handling-webpack
       // "chain" is a webpack-chain object https://github.com/neutrinojs/webpack-chain
       
-      chainWebpack (chain) {
-        chain.plugin('eslint-webpack-plugin')
-          .use(ESLintPlugin, [{ extensions: [ 'js', 'vue' ] }])
-      }
+      //chainWebpack (chain) {
+      //  chain.plugin('eslint-webpack-plugin')
+      //    .use(ESLintPlugin, [{ extensions: [ 'js', 'vue' ] }])
+      //}
       
     },
 
